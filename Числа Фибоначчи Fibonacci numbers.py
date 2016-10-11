3fa329b61e6bab4a82bb79be22484724e136e526
Nataliya Sashnikova <sashnat@yahoo.com>
2016-10-11 10:52:22 +0300

    Add files via upload

---
+++ Числа Фибоначчи Fibonacci numbers.py
@@ -0,0 +1,13 @@
+# sashnat. Функции и рекурсия / Functions and recursion
+# «Числа Фибоначчи» "Fibonacci numbers"
+# Напишите функцию fib(n), которая по данному целому неотрицательному n возвращает n-e число Фибоначчи.
+# В этой задаче нельзя использовать циклы — используйте рекурсию.
+
+def fin(n):
+    if n == 1 or n == 2:
+        return 1
+    elif n == 3:
+        return 2
+    else:
+        return fin(n - 1) + fin(n -2)
+print(fin(int(input())))
